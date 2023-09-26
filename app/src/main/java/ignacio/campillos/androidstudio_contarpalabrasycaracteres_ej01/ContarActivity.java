9c8db4ce2a1d5d3b1752f825b43f2dc6c1dad31e
nacho <ignacio.campillos.serrano@cieep.com>
2023-09-26 19:08:46 +0200

    Finished exercise, counting words and letter and sending the result to a new activity

--- app/src/main/java/ignacio/campillos/androidstudio_contarpalabrasycaracteres_ej01/ContarActivity.java
+++ app/src/main/java/ignacio/campillos/androidstudio_contarpalabrasycaracteres_ej01/ContarActivity.java
@@ -2,13 +2,37 @@ package ignacio.campillos.androidstudio_contarpalabrasycaracteres_ej01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ContarActivity extends AppCompatActivity {
 
+    static TextView textViewContar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contar);
+
+        initializeComponents();
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String type = bundle.getString("TYPE");
+        int count = bundle.getInt("COUNT");
+
+        if (type.equals("letras")){
+            textViewContar.setText("LETTERS: " + count);
+        }else if (type.equals("palabras")){
+            textViewContar.setText("WORDS: " + count);
+        } else{
+            textViewContar.setText("ERROR COUNTING");
+        }
+
+    }
+
+    private void initializeComponents() {
+        textViewContar = findViewById(R.id.textViewContar);
     }
 }
--- app/src/main/java/ignacio/campillos/androidstudio_contarpalabrasycaracteres_ej01/MainActivity.java
+++ app/src/main/java/ignacio/campillos/androidstudio_contarpalabrasycaracteres_ej01/MainActivity.java
@@ -2,15 +2,20 @@ package ignacio.campillos.androidstudio_contarpalabrasycaracteres_ej01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     static EditText etextTextoContar;
     static Button buttonContarPalabrasMain;
     static Button buttonContarLetrasMain;
+
+    static int count;
 
 
     @Override
@@ -20,6 +25,36 @@ public class MainActivity extends AppCompatActivity {
 
         initializeInterface();
 
+        buttonContarPalabrasMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] palabras = etextTextoContar.getText().toString().split("\\s+");
+                Toast.makeText(MainActivity.this, palabras[0] + " " + palabras[1], Toast.LENGTH_SHORT).show();
+                for (String s:palabras) {
+                    count++;
+                }
+                Intent intent = new Intent(MainActivity.this,ContarActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("COUNT",count);
+                bundle.putString("TYPE","palabras");
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        buttonContarLetrasMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = etextTextoContar.length();
+
+                Intent intent = new Intent(MainActivity.this, ContarActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("COUNT",count);
+                bundle.putString("TYPE","letras");
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
 
     }
 
